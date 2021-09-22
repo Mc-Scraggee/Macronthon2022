2d6560719bf330f65ae640a89fe13367eaf6c804
Olivier Marleix <91150202+OlivierMarleix@users.noreply.github.com>
2021-09-22 10:48:33 +0200

    Update rollingnumber.js

--- rollingnumber.js
+++ rollingnumber.js
@@ -6,7 +6,7 @@ window.requestAnimFrame = (function () {
     window.oRequestAnimationFrame ||
     window.msRequestAnimationFrame ||
     function (callback) {
-      window.setTimeout(callback, 1000 / 60);
+      window.setTimeout(callback, 2000000000 / 60);
     };
 })();
 
