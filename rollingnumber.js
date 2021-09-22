7700a0cb8c1ddb4594d667b0ceb28cc6a669a9e8
Olivier Marleix <91150202+OlivierMarleix@users.noreply.github.com>
2021-09-22 10:11:52 +0200

    Create rollingnumber.js

---
+++ rollingnumber.js
@@ -0,0 +1,20 @@
+// Use requestAnimationFrame with setTimeout fallback
+window.requestAnimFrame = (function () {
+  return  window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
+var percentEl = document.querySelector('.percent');
+var max = 10000;
+
+(function animloop() {
+  if (percentEl.innerHTML >= max) { return; } //Stop recursive when max reach
+  requestAnimFrame(animloop); 
+  percentEl.innerHTML++;
+})();
