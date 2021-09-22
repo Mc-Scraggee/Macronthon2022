c64a4668fae87b3d60059acd1dc6d5f857d1866e
Olivier Marleix <91150202+OlivierMarleix@users.noreply.github.com>
2021-09-22 10:13:37 +0200

    Update rollingnumber.js

--- rollingnumber.js
+++ rollingnumber.js
@@ -11,7 +11,7 @@ window.requestAnimFrame = (function () {
 })();
 
 var percentEl = document.querySelector('.percent');
-var max = 10000;
+var max = 100;
 
 (function animloop() {
   if (percentEl.innerHTML >= max) { return; } //Stop recursive when max reach
